de4d797ee98bba7cb745dee0eeb0522a597f5ae2
Burak Sürmen <busurmen@gmail.com>
2020-03-05 11:52:33 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,32 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-const Review = ({
-  headline,
-  byline,
-  link,
-  summary_short
-}) => {
-  return (
-    <div key={headline} className="review">
-    <header>
-    <a
-     className="review-link"
-     href={link.url}
-     >
-     {headline}
-     </a>
-     <br/>
-    <span className="author">{byline}</span>
-    </header>
-    <blockquote>{summary_short}</blockquote>
-    </div>
-  )
+export default function MovieReviews(props) {
+    return (
+        <div className="review-list">
+            {
+                props.reviews.map((el)=>
+                {return <div key={Math.random()} className="review">{el.display_title}</div>}
+                )
+            }
+        </div>
+    )
 }
-
-const MovieReviews = ({reviews}) => <div className="review-list">{reviews.map(Review)}</div>
-
-MovieReviews.defaultProps = {
-  reviews: []
-}
-
-export default MovieReviews
